b8e1b053fee7913d4b5bb246cecba5aee3276ac6
Juan <juanpa_solano@hotmail.com>
2016-05-03 21:03:07 -0500

    fixing general styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+require('./favicon.ico');
+import './styles/styles.scss';
 
 class App extends Component {
 	render () {
