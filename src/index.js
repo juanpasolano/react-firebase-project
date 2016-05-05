7eeaaf0be660a81c77f8e46fdde76319a5ea2657
Juan <juanpa_solano@hotmail.com>
2016-05-04 20:04:13 -0500

    simple router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import App from './components/app/app';
 
 require('./favicon.ico');
 import './styles/styles.scss';
 
 
-render(<App/>, document.getElementById('app'));
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app'));
