322e5fb509bc57881bc01eb63d26f0ffa5fe3b74
Juan <juanpa_solano@hotmail.com>
2016-05-14 22:16:05 -0500

    setting up nightwatch

---
+++ tests/testLogin.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name=email]', 'student@rta.com')
+      .setValue('input[name=password]', '123123123')
+      .waitForElementVisible('button[type=submit]', 1000)
+      .click('button[type=submit]')
+      .pause(1000)
+  }
+};
+
